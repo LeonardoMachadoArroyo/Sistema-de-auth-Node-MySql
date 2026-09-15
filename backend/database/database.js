0c37c199be8c66e2ac94b0f0689b42ccfbe713f4
lucym2008 <leonardomachadoarroyo14@gmail.com>
2026-09-15 14:02:55 -0300

    testanto upar o projeto

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,8 +1,13 @@
 import mysql from 'mysql2/promise'
 
+console.log('DB_HOST:', process.env.DB_HOST)
+console.log('DB_PORT:', process.env.DB_PORT)
+console.log('DB_USER:', process.env.DB_USER)
+console.log('DB_NAME:', process.env.DB_NAME)
+
 const db = mysql.createPool({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
@@ -14,10 +19,6 @@ const db = mysql.createPool({
 try {
     await db.query('SELECT 1')
     console.log('MySQL Aiven conectado com sucesso!')
-    console.log('DB_HOST:', process.env.DB_HOST)
-    console.log('DB_PORT:', process.env.DB_PORT)
-    console.log('DB_USER:', process.env.DB_USER)
-    console.log('DB_NAME:', process.env.DB_NAME)
 } catch (erro) {
     console.error('ERRO AO CONECTAR AO MYSQL:', erro)
 }
